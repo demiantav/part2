a14b9c032557cd6a12bf55a19d5576d2c7032917
Demian Tavolaro <demiantavolaro6@gmail.com>
2025-02-25 15:37:14 -0300

    Using MaterialUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,67 +1,73 @@
-import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
-import {Table, Button, Form} from 'react-bootstrap'
+import ReactDOM from 'react-dom/client'
+import { useState } from 'react'
 
 import {
-  BrowserRouter,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
+  AppBar,
+  Toolbar,
+  Alert
+} from '@mui/material'
+
+
+import {
+  BrowserRouter as Router,
   Routes,
   Route,
   Link,
   Navigate,
-  useParams,
   useNavigate,
-} from 'react-router-dom';
+  useMatch
+} from "react-router-dom"
+
 
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-      been the industry standard dummy text ever since the 1500s, when an unknown printer took a
-      galley of type and scrambled it to make a type specimen book. It has survived not only five
-      centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-      It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-      passages, and more recently with desktop publishing software like Aldus PageMaker including
-      versions of Lorem Ipsum.
-    </p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </div>
-);
+)
 
-const Note = ({ notes }) => {
-  const id = useParams().id;
-  const note = notes.find((n) => n.id === Number(id));
+const Note = ({ note }) => {
+
   return (
     <div>
       <h2>{note.content}</h2>
       <div>{note.user}</div>
-      <div>
-        <strong>{note.important ? 'important' : ''}</strong>
-      </div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
     </div>
-  );
-};
+  )
+}
 
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <Table striped>
-      <tbody>
-        {notes.map(note =>
-          <tr key={note.id}>
-            <td>
-              <Link to={`/notes/${note.id}`}>
-                {note.content}
-              </Link>
-            </td>
-            <td>
-              {note.user}
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </Table>
+
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map(note => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>
+                {note.user}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
-);
+)
 
 const Users = () => (
   <div>
@@ -72,51 +78,36 @@ const Users = () => (
       <li>Arto Hellas</li>
     </ul>
   </div>
-);
+)
 
 const Login = (props) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const onSubmit = (event) => {
-    event.preventDefault();
-    props.onLogin('mluukkai');
-    navigate('/');
-  };
+    event.preventDefault()
+    props.onLogin('mluukkai')
+    navigate('/')
+  }
 
   return (
     <div>
       <h2>login</h2>
-      <Form onSubmit={onSubmit}>
-
-        <Form.Group>
-          <Form.Label>username:</Form.Label>
-          <Form.Control
-            type="text"
-            name="username"
-          />
-          <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>password:</Form.Label>
-          <Form.Control
-            type="password"
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-        <Button variant="primary" type="submit">
-          login
-        </Button>
-      </Form>
+      <form onSubmit={onSubmit}>
+        <div>
+          <TextField label="username" variant='filled' />
+        </div>
+        <div>
+          <TextField label="password" type='password' variant='filled' />
+        </div>
+        <div>
+          <Button variant="contained" color="primary" type="submit">
+            login
+          </Button>
+        </div>
+      </form>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -124,69 +115,85 @@ const App = () => {
       id: 1,
       content: 'HTML is easy',
       important: true,
-      user: 'Matti Luukkainen',
+      user: 'Matti Luukkainen'
     },
     {
       id: 2,
       content: 'Browser can execute only JavaScript',
       important: false,
-      user: 'Matti Luukkainen',
+      user: 'Matti Luukkainen'
     },
     {
       id: 3,
       content: 'Most important methods of HTTP-protocol are GET and POST',
       important: true,
-      user: 'Arto Hellas',
-    },
-  ]);
+      user: 'Arto Hellas'
+    }
+  ])
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const match = useMatch('/notes/:id')
+
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
 
   const login = (user) => {
-    setUser(user);
-  };
+    setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
+  }
 
   const padding = {
-    padding: 5,
-  };
+    padding: 5
+  }
 
   return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Link style={padding} to="/">
+    <Container>
+      <div>
+        {(message &&
+          <Alert severity="success">
+            {message}
+          </Alert>
+        )}
+      </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
             home
-          </Link>
-          <Link style={padding} to="/notes">
+          </Button>
+          <Button color="inherit" component={Link} to="/notes">
             notes
-          </Link>
-          <Link style={padding} to="/users">
+          </Button>
+          <Button color="inherit" component={Link} to="/users">
             users
-          </Link>
-          {user ? (
-            <em>{user} logged in</em>
-          ) : (
-            <Link style={padding} to="/login">
+          </Button>
+          {user
+            ? <em>{user} logged in</em>
+            : <Button color="inherit" component={Link} to="/login">
               login
-            </Link>
-          )}
-        </div>
+            </Button>
+          }
+        </Toolbar>
+      </AppBar>
 
-        <Routes>
-          <Route path="/notes/:id" element={<Note notes={notes} />} />
-          <Route path="/notes" element={<Notes notes={notes} />} />
-          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
-          <Route path="/login" element={<Login onLogin={login} />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-
+      <Routes>
+        <Route path="/notes/:id" element={<Note note={note} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login onLogin={login} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <div>
         <br />
-        <em>Note app, Department of Computer Science 2023</em>
+        <em>Note app, Department of Computer Science 2022</em>
       </div>
-    </div>
-  );
-};
+    </Container>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
